93170e41839e2fd505f63b7497b3ae531a237746
Will Brown <williambrown@williams-air.lan>
2019-07-17 23:50:41 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var shoutouts = [
+  "You are so helpful and your end product is always amazing.",
+  "Your aesthetic and ability to always help out scholars! I also love the vines you chose XD",
+  "you were super helpful when everyone was asking you for help with github :)",
+  "how willing you are to help other people. Your kindness goes a long way.",
+  "I love the sister site you made for me! You are also super talented at Koding.",
+  "that you are so willing to help those around you",
+  "You're very sweet! Also you are so creative!",
+  "how helpful you are before and after you finish your own project. You're always helping others to make sure nobody gets left behind :)"
+  "that you are always really willing to collaborate and help people.",
+  "How smart you are and how easy it is to work with you!",
+  "Your ability to work on each project with so much dedication.",
+  "how smart and kind you are. I remember how badly I was struggling on my project and you were so open to helping me and eventually helped me solved my bugs. I also always admire your projects.",
+  "You're always paying close attention to everyone and looking the way of how to help others in your free time",
+  "I love how you are also so helpful to anyone and everyone and don’t mind taking time to make sure that others’ problems are fixed."
+]
+
+for (x of shoutouts) {
+  $("#shoutout-container").append("<p>" + x + "</p> <br>");
+}
